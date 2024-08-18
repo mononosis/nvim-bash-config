1ea3bfdde371c24d725ee21f81eda26844d75eaa
Your Name <you@example.com>
2024-08-18 11:06:46 +0000

    test

--- lua/nvim-bash-config.lua
+++ lua/nvim-bash-config.lua
@@ -1,3 +1,2 @@
 my_init.source_me()
-require("misc").setup()
-my_init.extend_environment_from_lab()
+require("bash-lspconfig").setup()
--- lua/plugins/bash-lspconfig/init.lua
+++ lua/plugins/bash-lspconfig/init.lua
@@ -1,6 +1,9 @@
 local M = {}
+
+local lspconfig = require('lspconfig')
+
 function M.setup()
-  local lspconfig = require('lspconfig')
+
   local caps = vim.tbl_deep_extend(
     'force',
     vim.lsp.protocol.make_client_capabilities(),
@@ -14,16 +17,19 @@ function M.setup()
     autostart = true,
     on_attach = function(_, bufnr)
       -- Custom on_attach logic
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fl', '<cmd>lua vim.lsp.buf.format()<CR>',
-        { noremap = true, silent = true })
       vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>t', '<cmd>!bash %<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>fl",
+        ":!shfmt -i 2 -w %<CR>",
+        { noremap = true, silent = true }
+      )
     end
     ,
     capabilities = caps,
     cmd = { "bash-language-server", "start" },
     settings = {
-      ['nil'] = {
-        testSetting = 42,
+      bash = {
         formatting = {
           command = { "shfmt" },
         },
